d18d113e167bfc50d5bde2204216c59ea7554e00
idarciooliveira <idarciooliveira@gmail.com>
2023-09-25 09:48:54 +0100

    expo router config

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
+import 'expo-router/entry'
+import { router } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity onPress={() => router.push('/onboarding')}>
+        <Text>
+          Go
+        </Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
---
+++ app/onboarding/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function OnBoarding() {
+    return (
+        <View>
+            <Text>OnBoarding</Text>
+        </View>
+    )
+}
